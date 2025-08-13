e1dc6e441d3478fba24141e4a03af4e0b396ffb2
Slavencer <javiercarrascoarango@gmail.com>
2025-08-13 13:33:33 +0200

    feat: add project collections and fix homepage

---
+++ src/content.config.ts
@@ -0,0 +1,18 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const projects = defineCollection({ 
+    loader: glob({ pattern: "**/*.md", base: "./src/projects" }),
+    schema: z.object({
+        title: z.string(),
+        image: z.string().url().or(z.string()), // allow relative or full URL
+        description: z.string().max(300),
+    }),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { projects };
