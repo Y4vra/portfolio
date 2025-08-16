0b18a6457f8180379872007fa0a31d473ba7c103
Slavencer <javiercarrascoarango@gmail.com>
2025-08-16 21:08:27 +0200

    fix: github pages publication

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from 'astro/config';
 
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site:'https://slavencer.github.io',
+    base:'porfolio',
+    trailingSlash: 'ignore',
+});
