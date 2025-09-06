39a552a9146878183a9781ee6fe9efb23fa22c14
Slavencer <javiercarrascoarango@gmail.com>
2025-09-06 20:26:59 +0200

    style: sidebar motion and visual project fix

--- src/components/NavigationComponent.jsx
+++ src/components/NavigationComponent.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 export default function NavBar() {
   const [activeHash, setActiveHash] = useState("#start");
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(true);
 
   useEffect(() => {
     setActiveHash(window.location.hash || '#start');
@@ -13,10 +13,22 @@ export default function NavBar() {
     window.addEventListener('hashchange', onHashChange);
     return () => window.removeEventListener('hashchange', onHashChange);
   }, []);
+  const toggleSidebar =()=>{
+    setSidebarOpen(!sidebarOpen)
+
+    const main = document.querySelector('main');
+    if (!main) return;
+    if (sidebarOpen) {
+      main.classList.add('unshifted');
+    } else {
+      main.classList.remove('unshifted');
+    }
+  }
 
   return (
-    <nav class="navBar">
+    <nav class={sidebarOpen?"open navBar":"closed navBar"}>
       <h1><a class="nav-link" href="#start">Y4vra</a></h1>
+      <button id="sidebarToggle" onClick={toggleSidebar}></button>
       <a class={`nav-link ${activeHash === "#portfolio" ? "active button" : "button"}`} href="#portfolio">Projects</a>
       <a class={`nav-link ${activeHash === "#about" ? "active button" : "button"}`} href="#about">About</a>
       <a class={`nav-link ${activeHash === "#contact" ? "active button" : "button"}`} href="#contact">Contact</a>
