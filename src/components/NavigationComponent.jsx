2b28abaa9501958f047dff064fec06ce982cf221
Slavencer <javiercarrascoarango@gmail.com>
2025-11-18 13:59:47 +0100

    fix: fixes theme configuration

--- src/components/NavigationComponent.jsx
+++ src/components/NavigationComponent.jsx
@@ -12,6 +12,9 @@ export default function NavBar() {
     };
 
     window.addEventListener('hashchange', onHashChange);
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.setAttribute("data-theme", "dark");
+    }
     const sections = document.querySelectorAll('section[id]');
     const observer = new IntersectionObserver(
       (entries) => {
@@ -51,13 +54,49 @@ export default function NavBar() {
     }
   }
 
+  const toggleTheme = ()=>{
+    document.documentElement.toggleAttribute("data-theme", "dark");
+    btn = document.getElementById("sidebarToggle");
+    isDark = document.documentElement.getAttribute("data-theme") === "dark";
+    btn.dataset.on = isDark;
+    btn.setAttribute("aria-pressed", String(isDark));
+  }
+
   return (
-    <nav class={sidebarOpen?"open navBar":"closed navBar"}>
-      <h1><a class="nav-link" href="#start">Y4vra</a></h1>
-      <button id="sidebarToggle" onClick={toggleSidebar}></button>
-      <a onClick={linkClickSidebarCheck} class={`nav-link ${activeHash === "#portfolio" ? "active button" : "button"}`} href="#portfolio">Projects</a>
-      <a onClick={linkClickSidebarCheck} class={`nav-link ${activeHash === "#about" ? "active button" : "button"}`} href="#about">About</a>
-      <a onClick={linkClickSidebarCheck} class={`nav-link ${activeHash === "#contact" ? "active button" : "button"}`} href="#contact">Contact</a>
+    <nav className="
+        flex items-center justify-between
+        px-4 py-3
+        bg-bgLight
+        dark:bg-bgDark
+        text-main-900
+        border-b border-main-200/20
+      ">
+      <h1 className="text-xl font-semibold text-accent-500"><a href="#start">Y4vra</a></h1>
+      
+      {/*Desktop*/}
+      <div className="flex items-center gap-6">
+        <a onClick={linkClickSidebarCheck} className={`px-2 py-1 rounded-md text-accent-400 transition-colors ${activeHash === "#portfolio" ? "text-accent-500 font-medium" : "text-main-700 hover:text-accent-500"}`} href="#portfolio">Projects</a>
+        <a onClick={linkClickSidebarCheck} className={`px-2 py-1 transition-colors ${activeHash === "#about" ? "text-accent-500 font-medium" : "text-main-700 hover:text-accent-500"}`} href="#about">About</a>
+        <a onClick={linkClickSidebarCheck} className={`px-2 py-1 transition-colors ${activeHash === "#contact" ? "text-accent-500 font-medium" : "text-main-700 hover:text-accent-500"}`} href="#contact">Contact</a>
+        <label className="inline-flex items-center cursor-pointer">
+          <svg className="w-5 h-5 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.5 8.43A4.985 4.985 0 0 1 19 12a4.984 4.984 0 0 1-1.43 3.5M14 6.135v11.73a1 1 0 0 1-1.64.768L8 15H6a1 1 0 0 1-1-1v-4a1 1 0 0 1 1-1h2l4.36-3.633a1 1 0 0 1 1.64.768Z"/></svg>
+          <input type="checkbox" value="" className="sr-only peer"/>
+          <div className="relative mx-3 w-9 h-5 bg-neutral-quaternary peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-brand"></div>
+          <svg className="w-5 h-5 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.5 8.43A4.985 4.985 0 0 1 17 12a4.984 4.984 0 0 1-1.43 3.5m2.794 2.864A8.972 8.972 0 0 0 21 12a8.972 8.972 0 0 0-2.636-6.364M12 6.135v11.73a1 1 0 0 1-1.64.768L6 15H4a1 1 0 0 1-1-1v-4a1 1 0 0 1 1-1h2l4.36-3.633a1 1 0 0 1 1.64.768Z"/></svg>
+        </label>
+      </div>
+      {/*Mobile*/}
+      <button
+        id="sidebarToggle"
+        onClick={toggleSidebar}
+        className="hidden text-main-900 dark:text-main-50"
+      >
+        <svg className="w-6 h-6" stroke="currentColor" fill="none">
+          <path strokeLinecap="round" strokeLinejoin="round"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </button>
     </nav>
   );
 }
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
